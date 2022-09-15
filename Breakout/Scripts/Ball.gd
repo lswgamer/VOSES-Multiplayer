a415c3f47925296dcf9cb79530925a08862328bf
lswgamer <lsw369@yahoo.com.br>
2022-09-15 03:24:49 -0300

    Fix Level Walls

--- Breakout/Scripts/Ball.gd
+++ Breakout/Scripts/Ball.gd
@@ -13,5 +13,14 @@ func _process(delta):
 	position += velocity * delta
 
 func _on_Node2D_area_entered(area):
-	if area.name == "Lower_Bar" or "Upper_Bar":
-		velocity = -velocity
+	if (area.name == "Lower_Bar") or (area.name == "Upper_Bar"):
+		cs.shape.collide_and_get_contacts(cs.transform, area.cs.shape, area.cs.transform)
+		#print(area.cs.shape.collide_and_get_contacts(area.cs.transform, cs.shape, cs.transform))
+		velocity.y = -velocity.y
+		#print(cs.shape, cs.transform, area.cs.shape, area.cs.transform)
+		velocity.x = -100 #metade da barra pra esquerda a gente diminui a velocidade entre -1 e 0, metade da barra pra direita a gente aumenta a velocidade entre 0 e 1
+		#velocity = -velocity
+	if (area.name == "Left_Wall") or (area.name == "Right_Wall"):
+		print(area.name)
+		velocity.x = -velocity.x
+		
