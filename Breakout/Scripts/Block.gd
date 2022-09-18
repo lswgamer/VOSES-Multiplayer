77e1163b48f497a57093c714d03b0a1437e68282
lswgamer <lsw369@yahoo.com.br>
2022-09-18 13:29:46 -0300

    Implement Block Logic
    Blocks now have collision and interact with Ball.

---
+++ Breakout/Scripts/Block.gd
@@ -0,0 +1,44 @@
+extends Sprite
+
+
+export var hp = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func remove():
+	#play break animation
+	self.queue_free()
+
+
+
+
+func _on_Up_area_entered(area):
+	if (area.name == "Ball"):
+		area.velocity.y = -area.velocity.y
+		hp -= hp
+		if hp == 0:
+			remove()
+
+func _on_Down_area_entered(area):
+	if (area.name == "Ball"):
+		area.velocity.y = -area.velocity.y
+		hp -= hp
+		if hp == 0:
+			remove()
+		
+func _on_Left_area_entered(area):
+	if (area.name == "Ball"):
+		area.velocity.x = -area.velocity.x
+		hp -= hp
+		if hp == 0:
+			remove()
+		
+func _on_Right_area_entered(area):
+	if (area.name == "Ball"):
+		area.velocity.x = -area.velocity.x
+		hp -= hp
+		if hp == 0:
+			remove()
